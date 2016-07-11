cc1ba4bda0d1ffcdd5aeb1f628f8eb60b74941cb
Zac <zknoebel@gmail.com>
2016-07-10 16:23:55 -1000

    restoring accidentally deleted or changed files as much as possible 	modified:   .classpath 	modified:   bin/ClassOption.class 	modified:   bin/Main.class 	modified:   bin/classRoster.txt 	modified:   src/ClassOption.java 	modified:   src/Main.java 	new file:   src/classRoster.txt

--- src/ClassOption.java
+++ src/ClassOption.java
@@ -1,41 +1,44 @@
 
 public class ClassOption {
 	private String cn;
-	private int ms1, me1, ts1, te1, ws1, we1, rs1, re1, fs1, fe1, ms2, me2, ts2, te2, ws2, we2, rs2, re2, fs2, fe2;
+	private int crn, ms1, me1, ts1, te1, ws1, we1, rs1, re1, fs1, fe1
+	, ms2, me2, ts2, te2, ws2, we2, rs2, re2, fs2, fe2;
 	private int weekLength = 5;
-	private int time = 148;
+	private int time = 144;
 	int[][] weeklyTimes = new int[5][148];
 
-	ClassOption(String className, int monStart1, int monEnd1,
-			int tuesStart1, int tuesEnd1, 
-			int wedStart1, int wedEnd1,
+	ClassOption(String className, int courseRegistrationNumber,
+			int monStart1,   int monEnd1,
+			int monStart2,   int monEnd2,
+			int tuesStart1,  int tuesEnd1, 
+			int tuesStart2,  int tuesEnd2, 
+			int wedStart1,   int wedEnd1,
+			int wedStart2,   int wedEnd2,
 			int thursStart1, int thursEnd1,
-			int friStart1, int friEnd1,
-			int monStart2, int monEnd2,
-			int tuesStart2, int tuesEnd2, 
-			int wedStart2, int wedEnd2,
 			int thursStart2, int thursEnd2,
-			int friStart2, int friEnd2){
+			int friStart1,   int friEnd1,
+			int friStart2,   int friEnd2){
+		crn = courseRegistrationNumber;
 		ms1 = monStart1;
 		me1 = monEnd1;
+		ms2 = monStart2;
+		me2 = monEnd2;
 		ts1 = tuesStart1;
 		te1 = tuesEnd1;
+		ts2 = tuesStart2;
+		te2 = tuesEnd2;
 		ws1 = wedStart1;
 		we1 = wedEnd1;
+		ws2 = wedStart2;
+		we2 = wedEnd2;
 		rs1 = thursStart1;
 		re1 = thursEnd1;
+		rs2 = thursStart2;
+		re2 = thursEnd2;
 		fs1 = friStart1;
 		fe1 = friEnd1;
-		ms1 = monStart2;
-		me1 = monEnd2;
-		ts1 = tuesStart2;
-		te1 = tuesEnd2;
-		ws1 = wedStart2;
-		we1 = wedEnd2;
-		rs1 = thursStart2;
-		re1 = thursEnd2;
-		fs1 = friStart2;
-		fe1 = friEnd2;
+		fs2 = friStart2;
+		fe2 = friEnd2;
 		cn = className;
 		//creates matrix down and over style
 		for(int i = 0; i < weekLength; i ++){
@@ -105,7 +108,12 @@ public class ClassOption {
 	void printName(){
 		System.out.println(cn);
 	}
-	int list(int row, int column){
+	void printCRN(){
+		if (crn != 0){
+			System.out.println(crn);
+		}
+	}
+	int ls(int row, int column){
 		return weeklyTimes[row][column];
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,19 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args)throws java.io.IOException{
+		int emptyCount = 0;
 		int weekLength = 5;
-		int time = 148;
+		int time = 144;
 		boolean overlap = false;
 		Scanner scanner = new Scanner(new FileReader("src//classRoster.txt"));
+		int[] numberOfClasses = new int[10];
+		for(int i = 0; i < 10; i ++){
+			numberOfClasses[i] = 10;
+		}
+
 		int[][] week = new int[5][148];
-		for(int i = 0; i < 5; i ++){
-			for(int j = 0; j < 148; j ++){
+		for(int i = 0; i < weekLength; i ++){
+			for(int j = 0; j < time; j ++){
 				week[i][j] = 0;
 			}
 		}
@@ -17,66 +23,276 @@ public class Main {
 		ClassOption[][] list = new ClassOption[10][10];
 		for(int i = 0; i < 10; i ++){
 			for(int j = 0; j < 10; j ++){
-			list[i][j] = new ClassOption(
-					scanner.next(),
-					scanner.nextInt(),
-					scanner.nextInt(),
-					scanner.nextInt(),
-					scanner.nextInt(),
-					scanner.nextInt(),
-					scanner.nextInt(),
-					scanner.nextInt(),
-					scanner.nextInt(),
-					scanner.nextInt(),
-					scanner.nextInt(),
-					scanner.nextInt(),
-					scanner.nextInt(),
-					scanner.nextInt(),
-					scanner.nextInt(),
-					scanner.nextInt(),
-					scanner.nextInt(),
-					scanner.nextInt(),
-					scanner.nextInt(),
-					scanner.nextInt(),
-					scanner.nextInt()
-					);
+				list[i][j] = new ClassOption(
+						scanner.next(),
+						scanner.nextInt(),
+						scanner.nextInt(),
+						scanner.nextInt(),
+						scanner.nextInt(),
+						scanner.nextInt(),
+						scanner.nextInt(),
+						scanner.nextInt(),
+						scanner.nextInt(),
+						scanner.nextInt(),
+						scanner.nextInt(),
+						scanner.nextInt(),
+						scanner.nextInt(),
+						scanner.nextInt(),
+						scanner.nextInt(),
+						scanner.nextInt(),
+						scanner.nextInt(),
+						scanner.nextInt(),
+						scanner.nextInt(),
+						scanner.nextInt(),
+						scanner.nextInt(),
+						scanner.nextInt()
+						);
+			}
 		}
-		}
-		for(int f = 0; f < 10; f ++){
-		for(int g = 0; g < 10; g ++){
-			for(int h = 0; h < 10; h ++){
-				for(int i = 0; i < weekLength; i ++){
-					for(int j = 0; j < time; j ++){
-						week[i][j] += list[f][h].list(i,j) + list[f][g].list(i, j);
-
-					}
+		for(int i = 0; i < 10; i ++){
+			for(int j = 0; j < 10; j ++){
+				if(emptyCount == 0){
+					numberOfClasses[i] --;
 				}
-				for(int i = 0; i < weekLength; i ++){
-					for(int j = 0; j < time; j ++){
-						if(list[f][h].list(i,j) + list[f][g].list(i, j) > 1){
-							overlap = true;
-						}
-
-					}
-				}
-				if(!overlap){
-					for(int i = 0; i < weekLength; i ++){
-						for(int j = 0; j < time; j ++){
-							System.out.print(" " + week[i][j]);
-						}
-						System.out.println("");
-					}
-
-					System.out.println("");
-				}
-				overlap = false;
-				for(int i = 0; i < 5; i ++){
-					for(int j = 0; j < 148; j ++){
-						week[i][j] = 0;
+				emptyCount = 0;
+				for(int k = 0; k < weekLength; k ++){
+					for(int l = 0; l < time; l ++){
+						emptyCount += list[i][j].ls(k,l);
 					}
 				}
 			}
 		}
+		for(int i = 0; i < 10; i ++){
+			System.out.println(numberOfClasses[i]);
+		}
+		for(int a = 0; a < numberOfClasses[0]; a ++){
+			for(int k = 0; k < weekLength; k ++){
+				for(int l = 0; l < time; l ++){					
+					week[k][l] += list[0][a].ls(k,l);
+				}
+			}
+			for(int b = -1; b < numberOfClasses[1]; b ++){
+				if(numberOfClasses[1] > 0){
+					if(b == -1){
+						b = 0;
+					}
+					for(int k = 0; k < weekLength; k ++){
+						for(int l = 0; l < time; l ++){					
+							week[k][l] += list[1][b].ls(k,l);
+						}
+					}
+				}
+				for(int c = -1; c < numberOfClasses[2]; c ++){
+					if(numberOfClasses[2] > 0){
+						if(c == -1){
+							c = 0;
+						}
+						for(int k = 0; k < weekLength; k ++){
+							for(int l = 0; l < time; l ++){					
+								week[k][l] += list[2][c].ls(k,l);
+							}
+						}
+					}
+					for(int d = -1; d < numberOfClasses[3]; d ++){
+						if(numberOfClasses[3] > 0){
+							if(d == -1){
+								d = 0;
+							}
+							for(int k = 0; k < weekLength; k ++){
+								for(int l = 0; l < time; l ++){					
+									week[k][l] += list[3][d].ls(k,l);
+								}
+							}
+						}
+						for(int e = -1; e < numberOfClasses[4]; e ++){
+							if(numberOfClasses[4] > 0){
+								if(e == -1){
+									e = 0;
+								}
+								for(int k = 0; k < weekLength; k ++){
+									for(int l = 0; l < time; l ++){					
+										week[k][l] += list[4][e].ls(k,l);
+									}
+								}
+							}
+							for(int f = -1; f < numberOfClasses[5]; f ++){
+								if(numberOfClasses[5] > 0){
+									if(f == -1){
+										f = 0;
+									}
+									for(int k = 0; k < weekLength; k ++){
+										for(int l = 0; l < time; l ++){					
+											week[k][l] += list[5][f].ls(k,l);
+										}
+									}
+								}
+								for(int g = -1; g < numberOfClasses[6]; g ++){
+									if(numberOfClasses[6] > 0){
+										if(g == -1){
+											g = 0;
+										}
+										for(int k = 0; k < weekLength; k ++){
+											for(int l = 0; l < time; l ++){					
+												week[k][l] += list[6][g].ls(k,l);
+											}
+										}
+									}
+									for(int h = -1; h < numberOfClasses[7]; h ++){
+										if(numberOfClasses[7] > 0){
+											if(h == -1){
+												h = 0;
+											}
+											for(int k = 0; k < weekLength; k ++){
+												for(int l = 0; l < time; l ++){					
+													week[k][l] += list[7][h].ls(k,l);
+												}
+											}
+										}
+										for(int i = -1; i < numberOfClasses[8]; i ++){
+											if(numberOfClasses[8] > 0){
+												if(i == -1){
+													i = 0;
+												}
+												for(int k = 0; k < weekLength; k ++){
+													for(int l = 0; l < time; l ++){					
+														week[k][l] += list[8][i].ls(k,l);
+													}
+												}
+											}
+											for(int j = -1; j < numberOfClasses[9]; j ++){
+												if(numberOfClasses[9] > 0){
+													if(j == -1){
+														j = 0;
+													}
+													for(int k = 0; k < weekLength; k ++){
+														for(int l = 0; l < time; l ++){				
+															week[k][l] += list[9][j].ls(k,l);
+														}
+													}
+												}
+												for(int k = 0; k < weekLength; k ++){
+													for(int l = 0; l < time; l ++){				
+														if(week[k][l] > 1){
+															overlap = true;
+														}
+													}
+												}
+												if(!overlap){
+													if(a > -1){
+														list[0][a].printCRN();
+													}
+													if(b > -1){
+														list[1][b].printCRN();
+													}
+													if(c > -1){
+														list[2][c].printCRN();
+													}
+													if(d > -1){
+														list[3][d].printCRN();
+													}
+													if(e > -1){
+														list[4][e].printCRN();
+													}
+													if(f > -1){
+														list[5][f].printCRN();
+													}
+													if(g > -1){
+														list[6][g].printCRN();
+													}
+													if(h > -1){
+														list[7][h].printCRN();
+													}
+													if(i > -1){
+														list[8][i].printCRN();
+													}
+													if(j > -1){
+														list[9][j].printCRN();
+													}
+													for(int k = 0; k < weekLength; k ++){
+														for(int l = 0; l < time; l ++){			
+															System.out.print(week[k][l] + " ");
+														}
+														System.out.println("");
+													}
+												}
+												System.out.println("");
+												overlap = false;
+												if(j > -1){
+													for(int k = 0; k < weekLength; k ++){
+														for(int l = 0; l < time; l ++){					
+															week[k][l] -= list[9][j].ls(k,l);
+														}
+													}
+												}
+											}
+											if(i > -1){
+												for(int k = 0; k < weekLength; k ++){
+													for(int l = 0; l < time; l ++){					
+														week[k][l] -= list[8][i].ls(k,l);
+													}
+												}
+											}
+										}
+										if(h > -1){
+											for(int k = 0; k < weekLength; k ++){
+												for(int l = 0; l < time; l ++){					
+													week[k][l] -= list[7][h].ls(k,l);
+												}
+											}
+										}
+									}
+									if(g > -1){
+										for(int k = 0; k < weekLength; k ++){
+											for(int l = 0; l < time; l ++){					
+												week[k][l] -= list[6][g].ls(k,l);
+											}
+										}
+									}
+								}
+								if(f > -1){
+									for(int k = 0; k < weekLength; k ++){
+										for(int l = 0; l < time; l ++){					
+											week[k][l] -= list[5][f].ls(k,l);
+										}
+									}
+								}
+							}
+							if(e > -1){
+								for(int k = 0; k < weekLength; k ++){
+									for(int l = 0; l < time; l ++){					
+										week[k][l] -= list[4][e].ls(k,l);
+									}
+								}
+							}
+						}if(d > -1){
+							for(int k = 0; k < weekLength; k ++){
+								for(int l = 0; l < time; l ++){					
+									week[k][l] -= list[3][d].ls(k,l);
+								}
+							}
+						}
+					}
+					if(c > -1){
+						for(int k = 0; k < weekLength; k ++){
+							for(int l = 0; l < time; l ++){					
+								week[k][l] -= list[2][c].ls(k,l);
+							}
+						}
+					}
+				}
+				if(b > -1){
+					for(int k = 0; k < weekLength; k ++){
+						for(int l = 0; l < time; l ++){					
+							week[k][l] -= list[1][b].ls(k,l);
+						}
+					}
+				}
+			}
+			for(int k = 0; k < weekLength; k ++){
+				for(int l = 0; l < time; l ++){					
+					week[k][l] -= list[0][a].ls(k,l);
+				}
+			}
 		}
 		scanner.close();
 	}
