2b1b1ecf236475fe58df76ce2fdedf5bf871fedb
Zac <zknoebel@gmail.com>
2016-07-27 11:38:13 -1000

    modified:   bin/TxtFileCreator.class 	modified:   src/TxtFileCreator.java

--- src/TxtFileCreator.java
+++ src/TxtFileCreator.java
@@ -9,22 +9,24 @@ public class TxtFileCreator{
 	FileWriter writer;
 	int counter = 0;
 	boolean blank = true;
+	String CourseName;
+	int CourseRegistrationNumber,
+	 MonStart1,  MonFinish1,
+	 MonStart2,  MonFinish2,
+	 TuesStart1, TuesFinish1,
+	 TuesStart2, TuesFinish2,
+	 WedStart1, WedFinish1,
+	 WedStart2, WedFinish2,
+	 ThurStart1, ThurFinish1,
+	 ThurStart2, ThurFinish2,
+	 FriStart1, FriFinish1,
+	 FriStart2, FriFinish2;
 
 	TxtFileCreator()throws java.io.IOException{
 		writer = new FileWriter("TxtFile.txt");
 	}
 
-	void printClass(String CourseName, int CourseRegistrationNumber,
-			int MonStart1, int MonFinish1,
-			int MonStart2, int MonFinish2,
-			int TuesStart1, int TuesFinish1,
-			int TuesStart2, int TuesFinish2,
-			int WedStart1, int WedFinish1,
-			int WedStart2, int WedFinish2,
-			int ThurStart1, int ThurFinish1,
-			int ThurStart2, int ThurFinish2,
-			int FriStart1, int FriFinish1,
-			int FriStart2, int FriFinish2)throws java.io.IOException{
+	void printClass()throws java.io.IOException{
 		if(blank == true){
 			for(int i = 0; i < 10; i ++){
 				writer.write("abc 0 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1");
@@ -33,6 +35,10 @@ public class TxtFileCreator{
 			writer.write("something");
 			counter += 1;
 		}
+	}
+	void setCourseName(String courseName){
+		CourseName = courseName;
+		
 	}
 
 }
