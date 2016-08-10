1fc3807fcd764d223830f2ee1389d2ed60991bc8
zac <zknoebel@hawaii.edu>
2016-08-10 07:52:17 -1000

    creating a start window 	modified:   bin/InputWindow.class 	modified:   src/InputWindow.java

--- src/InputWindow.java
+++ src/InputWindow.java
@@ -1,7 +1,19 @@
+import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
-public class InputWindow {
+public class InputWindow extends JFrame{
+	private JLabel title = new JLabel();
+	private JTextArea warning = new JTextArea();
+	private JTextField[] classNameTxtField = new JTextField[10];
+	private JComboBox[][] classTimeComboBox = new JComboBox[10][28];
 	
+	public InputWindow(){
+
+	      Container cp = this.getContentPane();
+	      cp.setLayout(new FlowLayout());
+	      
+	}
 	
 
 }
